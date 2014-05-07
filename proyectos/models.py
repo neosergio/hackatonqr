de40ada7b873456b34fc03a2e5f3df76ba14ad70
Sergio Infante <rsinfante@gmail.com>
2014-05-07 16:05:13 -0500

    Modified Proyecto model so that have more data
    Additional data has been included so that project descriptions will be
    better.

--- proyectos/models.py
+++ proyectos/models.py
@@ -5,8 +5,13 @@ from django.db import models
 class Proyecto(models.Model):
     name = models.CharField(max_length=250, verbose_name='Nombre', unique=True)
     description = models.TextField(verbose_name='Descripción')
+    owner_name = models.TextField(verbose_name='Nombre del autor')
+    technology = models.TextField(verbose_name="Tecnología/Herramienta", blank=True, null=True)
+    category = models.CharField(max_length=250, verbose_name="Categoría", blank=True, null=True)
+    email = models.EmailField(verbose_name="E-mail", blank=True, null=True)
     max_participants = models.IntegerField(verbose_name='Cantidad máxima de participantes')
     status = models.CharField(max_length=1, choices=(('A','Abierto'),('C','Cerrado'),))
+    url = models.URLField(verbose_name="URL del repositorio", blank=True, null=True)
 
     def __unicode__(self):
         return self.name
