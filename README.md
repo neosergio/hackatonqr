38bd091c7fbb1969dc7432fa77e6fbd883e2f3d0
Sergio Infante <rsinfante@gmail.com>
2014-05-07 16:59:32 -0500

    First version of import projects data script

--- README.md
+++ README.md
@@ -8,8 +8,10 @@ Instructions
 ------------
 1. Use requirements.txt with pip
 2. Create the database
-  * Optional: Run import-data.py script if you want to test with csv and sqlite3
+  * Optional 1: Run import-data.py script if you want to test with csv and sqlite3
+  * Optional 2: Run import-projects-data.py script if you want to test with xls/xlsx files and sqlite3
+(csv and xls/xlsx files are not included in this repository, you must read the code and create samples)
 
 Note
 ----
-This is a simple application to handle participants in hackatons, is just an alfa version.
+This is a simple application to handle participants in hackatons, is just an beta version.
---
+++ import-projects-data.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+#-*- coding:utf-8 -*-
+
+#import modules
+import xlrd
+import os
+
+#files to import and temporaly generate sql file
+EXCEL_FILE = 'projects.xlsx'
+
+#open Excel file
+print "==== Open Excel file ===="
+import_file = xlrd.open_workbook(EXCEL_FILE)
+sheet = import_file.sheet_by_index(0)
+
+#create and open a temporally file to store sql queries
+print "==== Create a temporally file to store insertion queries ===="
+os.system('touch tmp_import.sql')
+import_sql_file = open('tmp_import.sql', 'a')
+
+#pass CSV to tmp_import.sql
+counter = 0
+for row in range(0, sheet.nrows):
+    counter += 1
+    # The Excel file must have these fields, without headers:
+    # name, description, owner_name, technology(optional), category(optional), email(optional)
+    # #max_participants(integer), status, url(optional)
+    query = """ insert into proyectos_proyecto values (null,"%s","%s","%s","%s","%s","%s","%s","%s");\n """
+    name = sheet.cell(row,0).value
+    description = sheet.cell(row,1).value
+    owner_name = sheet.cell(row,2).value
+    technology = sheet.cell(row,3).value
+    category = sheet.cell(row,4).value
+    email = sheet.cell(row,5).value
+    max_participants = sheet.cell(row,6).value
+    status = sheet.cell(row,7).value
+    url = sheet.cell(row,8).value
+    values = (name, description, owner_name, technology, category, email, max_participants, status, url)
+    print values
+    print "== Record %i ==" % (counter)
+    #import_sql_file.write(query)
+#close the temporally file  
+#import_sql_file.close()
+
+#execute the command to import
+print "==== Import insertion queries to the database ===="
+#COMMAND_TO_IMPORT = "sqlite3 db.sqlite3 < tmp_import.sql"
+#os.system(COMMAND_TO_IMPORT)
+
+print "==== Delete the temporally file ===="
+#delete temporally file
+#os.system('rm tmp_import.sql')
